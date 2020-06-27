88a7ddef699895d957e164b21292ebee5df04040
yukta <iit2019052@iiita.ac.in>
2020-06-27 23:18:34 +0530

    Added the location button

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
+
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void get_location()async
+  {
+    Position position = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
+            get_location();
+
             //Get the current location
           },
           child: Text('Get Location'),
